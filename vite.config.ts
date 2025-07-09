aacf50613a8a97b4851f736f1e3e4252548ca481
Aaron Gertler <aaron.gertler@gmail.com>
2025-07-09 14:17:01 -0500

    fix: changed from build to production

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === 'build' ? '/tl-proposal-calculator/' : '/',
+    base: mode === 'production' ? '/tl-proposal-calculator/' : '/',
   }
 })
