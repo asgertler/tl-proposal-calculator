ee6aa67d8c9921e7db71a7f43fcc5c3efa6a4d53
Aaron Gertler <aaron.gertler@gmail.com>
2025-07-09 14:43:26 -0500

    fix: updating config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
-  console.log('VITE build command:', command)
-  console.log('VITE build mode:', mode)
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/tl-proposal-calculator/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
 })
